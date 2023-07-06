b31813fbe16a298652aa14c1c3abaf870eca0bd3
Janaka L. Wijekoon (JaNa) <janaka.wijekoon@gmail.com>
2023-07-06 11:59:31 +0930

    Update Config.py

--- Config.py
+++ Config.py
@@ -3,6 +3,6 @@ class Config:
   SEARCHKEY = "Blister Blight and  Tea leafe and  Machine Learning"
   NUMRECORDS = 5
   
-  # Install the modules whic are missing
+  # Install the modules which are missing
   def install_module(module_name):
     subprocess.check_call(["pip", "install", module_name])
